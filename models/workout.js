ee0f78e57b4c2dfc6a10d22a7dc21632c8bd911f
Archangel <nathapcal@gmail.com>
2020-10-22 20:33:22 -0400

    added virtual to workout model

--- models/workout.js
+++ models/workout.js
@@ -1,19 +1,28 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true } };
 
-const WorkoutSchema = new Schema({
-  day: { type: Date, default: () => new Date() },
-  exercises: [
-    {
-      type: { type: String },
-      name: { type: String },
-      duration: { type: Number },
-      weight: { type: Number },
-      reps: { type: Number },
-      sets: { type: Number },
-      distance: { type: Number },
-    },
-  ],
+const WorkoutSchema = new Schema(
+  {
+    day: { type: Date, default: () => new Date() },
+    exercises: [
+      {
+        type: { type: String },
+        name: { type: String },
+        duration: { type: Number },
+        weight: { type: Number },
+        reps: { type: Number },
+        sets: { type: Number },
+        distance: { type: Number },
+      },
+    ],
+  },
+  opts
+);
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
