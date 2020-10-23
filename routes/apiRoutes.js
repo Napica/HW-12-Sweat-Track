b95534867ef2d89294ba884ad03a72297774ee2f
Archangel <nathapcal@gmail.com>
2020-10-22 20:11:53 -0400

    able to add/update data in the database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,7 +20,7 @@ module.exports = function (app) {
   });
 
   app.post("/api/workouts", function (req, res) {
-    // console.log(req.body)
+    console.log(req.body);
     db.Workout.create(req.body)
       .then((dbWorkout) => {
         res.json(dbWorkout);
@@ -29,4 +29,36 @@ module.exports = function (app) {
         res.json(err);
       });
   });
+
+
+  // logs new input
+  app.put("/api/workouts/:id", function (req, res) {
+    console.log(req.params.id);
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      {
+        $push: { exercises: req.body },
+      },
+      { new: true }
+    )
+      .then((dbExercise) => {
+        // console.log(dbExercise);
+        res.json(dbExercise)
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  // for testing
+  // app.put("/api/workouts/:id", function (req, res) {
+  //   console.log(req.params.id);
+  //   db.Workout.findByIdAndUpdate(req.params.id, { exercises: req.body })
+  //     .then((dbExercise) => {
+  //       console.log(dbExercise);
+  //     })
+  //     .catch((err) => {
+  //       res.json(err);
+  //     });
+  // });
 };
